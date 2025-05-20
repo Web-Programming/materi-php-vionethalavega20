addaafde3122355190e6003409fbc6887e0bd3fd
vionethalavega20 <vionetha2085@gmail.com>
2025-05-20 14:25:30 +0700

    Update index.blade.php

--- LATIHAN/resources/views/prodi/index.blade.php
+++ LATIHAN/resources/views/prodi/index.blade.php
@@ -3,105 +3,87 @@
 @section('title', "Halaman List Prodi")
 
 @section('content')
-        <!--begin::App Content Header-->
-        <div class="app-content-header">
-          <!--begin::Container-->
-          <div class="container-fluid">
-            <!--begin::Row-->
-            <div class="row">
-              <div class="col-sm-6"><h3 class="mb-0">Program Studi</h3></div>
-              <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-end">
-                  <li class="breadcrumb-item"><a href="{{ url("/") }}">Home</a></li>
-                  <li class="breadcrumb-item active" aria-current="page">Program Studi</li>
-                </ol>
-              </div>
-            </div>
-            <!--end::Row-->
-          </div>
-          <!--end::Container-->
-        </div>
-        <!--end::App Content Header-->
-        <!--begin::App Content-->
-        <div class="app-content">
-          <!--begin::Container-->
-          <div class="container-fluid">
-            <!--begin::Row-->
-            <div class="row">
-              <div class="col-12">
-                <!-- Default box -->
-                <div class="card">
-                  <div class="card-header">
-                    <h3 class="card-title">Program Studi</h3>
-                    <div class="card-tools">
-                      <button
-                        type="button"
-                        class="btn btn-tool"
-                        data-lte-toggle="card-collapse"
-                        title="Collapse"
-                      >
-                        <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
-                        <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
-                      </button>
-                      <button
-                        type="button"
-                        class="btn btn-tool"
-                        data-lte-toggle="card-remove"
-                        title="Remove"
-                      >
-                        <i class="bi bi-x-lg"></i>
-                      </button>
-                    </div>
-                  </div>
-                  <div class="card-body">
-                    <a href="{{ url('prodi/create') }}" class="btn btn-success" >Buat Prodi Baru</a>
-                    
-                    @if (session('status'))
-                      <div class="alert alert-success">
-                          {{ session('status') }}
-                      </div>
-                    @endif
+<!--begin::App Content Header-->
+<div class="app-content-header">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-6"><h3 class="mb-0">Program Studi</h3></div>
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-end">
+          <li class="breadcrumb-item"><a href="{{ url("/") }}">Home</a></li>
+          <li class="breadcrumb-item active" aria-current="page">Program Studi</li>
+        </ol>
+      </div>
+    </div>
+  </div>
+</div>
+<!--end::App Content Header-->
 
-                    <table class="table table-bordered table-striped">
-                      <thead>
-                        <tr>
-                          <th>No</th>
-                          <th>Nama Prodi</th>
-                          <th>Kode Prodi</th>
-                          <th>Aksi</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        @foreach ( $listprodi as $prodi)
-                          <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $prodi->nama }}</td>
-                            <td>{{ $prodi->kode_prodi }}</td>
-                            <td>
-                              <a href="{{ url('prodi/'.$prodi->id.'/edit') }}" >Edit</a>
-                              <form action="{{ url('prodi/'.$prodi->id) }}" method="post">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-link">Delete</button>
-                              </form>
-                            </td>
-                          </tr>
-                        @endforeach
-                      </tbody>                 
-                    </table>
-
-                  </div>
-                  <!-- /.card-body -->
-                  <div class="card-footer">Footer</div>
-                  <!-- /.card-footer-->
-                </div>
-                <!-- /.card -->
-              </div>
+<!--begin::App Content-->
+<div class="app-content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <!-- Default box -->
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">Program Studi</h3>
+            <div class="card-tools">
+              <button type="button" class="btn btn-tool" data-lte-toggle="card-collapse" title="Collapse">
+                <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
+                <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
+              </button>
+              <button type="button" class="btn btn-tool" data-lte-toggle="card-remove" title="Remove">
+                <i class="bi bi-x-lg"></i>
+              </button>
             </div>
-            <!--end::Row-->
           </div>
-          <!--end::Container-->
+          <div class="card-body">
+            <a href="{{ url('prodi/create') }}" class="btn btn-success mb-3">Buat Prodi Baru</a>
+            
+            @if (session('status'))
+              <div class="alert alert-success">
+                  {{ session('status') }}
+              </div>
+            @endif
+
+            <table class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Nama Prodi</th>
+                  <th>Kode Prodi</th>
+                  <th>Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($listprodi as $prodi)
+                  <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $prodi->nama }}</td>
+                    <td>{{ $prodi->kode_prodi }}</td>
+                    <td>
+                      <a href="{{ url('prodi/'.$prodi->id.'/edit') }}" class="btn btn-primary btn-sm">Edit</a>
+
+                      <form action="{{ url('prodi/'.$prodi->id) }}" method="POST" style="display:inline;" onsubmit="return confirm('Yakin ingin menghapus data ini?');">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                      </form>
+                    </td>
+                  </tr>
+                @endforeach
+              </tbody>                 
+            </table>
+
+          </div>
+          <div class="card-footer">Footer</div>
         </div>
-        <!--end::App Content-->
+      </div>
+    </div>
+  </div>
+</div>
+<!--end::App Content-->
 @endsection
+
 
